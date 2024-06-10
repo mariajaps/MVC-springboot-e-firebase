1460cbbd349a37c6f67d9d7b94841301b6285901
mariajaps <166346577+mariajaps@users.noreply.github.com>
2024-06-10 02:39:06 +0000

    Service order atualizado

--- ConCon/src/main/java/com/example/ConCon/service/OrderService.java
+++ ConCon/src/main/java/com/example/ConCon/service/OrderService.java
@@ -1,7 +1,37 @@
 package com.example.ConCon.service;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 @Service
-public class OrderService {
+public class FirebaseInitializer {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("path/to/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Firestore getFirebase() {
+        return FirestoreClient.getFirestore();
+    }
 }
