56e7fcc626ae91d72e8f125369b935315731d0f0
mariajaps <166346577+mariajaps@users.noreply.github.com>
2024-06-10 02:45:51 +0000

    service order atualizado

--- ConCon/src/main/java/com/example/ConCon/service/OrderService.java
+++ ConCon/src/main/java/com/example/ConCon/service/OrderService.java
@@ -1,37 +1,60 @@
 package com.example.ConCon.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+import com.example.ConCon.model.Order;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 
-import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Service
-public class FirebaseInitializer {
+public class OrderService {
+    @Autowired
+    FirebaseInitializer db;
 
-    @PostConstruct
-    public void initialize() {
-        try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("path/to/serviceAccountKey.json");
-
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com")
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public Order getOrder(int orderId) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = db.getFirebase().collection("Order").document(String.valueOf(orderId));
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+        Order order = null;
+        if (document.exists()) {
+            order = document.toObject(Order.class);
         }
+        return order;
     }
 
-    public Firestore getFirebase() {
-        return FirestoreClient.getFirestore();
+    public String createOrder(Order order) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionsApiFuture;
+        collectionsApiFuture = db.getFirebase().collection("Order").document(String.valueOf(order.getOderId())).set(order);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String updateOrder(Order order) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionsApiFuture;
+        collectionsApiFuture = db.getFirebase().collection("Order").document(String.valueOf(order.getOderId())).set(order);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String deleteOrder(int orderId) {
+        ApiFuture<WriteResult> writeResult = db.getFirebase().collection("Order").document(String.valueOf(orderId)).delete();
+        return "Successfully deleted order with ID: " + orderId;
+    }
+
+    public List<Order> getAllOrders() throws ExecutionException, InterruptedException {
+        List<Order> orderList = new ArrayList<>();
+        CollectionReference orders = db.getFirebase().collection("Order");
+        ApiFuture<QuerySnapshot> querySnapshot = orders.get();
+        for (DocumentSnapshot doc : querySnapshot.get().getDocuments()) {
+            Order order = doc.toObject(Order.class);
+            orderList.add(order);
+        }
+        return orderList;
     }
 }
