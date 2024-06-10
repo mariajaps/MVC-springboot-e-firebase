6522c3592951a3f0dbe070b82c0ca0bdd2811bd8
Maria Luiza Farias <130606221+aziul89@users.noreply.github.com>
2024-06-10 20:41:57 +0000

    cart controller

---
+++ ConCon/src/main/java/com/example/ConCon/controllers/CartController.java
@@ -0,0 +1,52 @@
+package com.example.ConCon.controllers;
+
+import com.example.ConCon.model.Cart;
+import com.example.ConCon.service.CartService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@Controller
+@CrossOrigin(origins = "*")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/cart")
+    public List<Cart> getAll() throws ExecutionException, InterruptedException {
+        return cartService.getCart();
+    }
+
+    @PostMapping("/createC")
+    public String create(@Valid @RequestBody Cart cart, BindingResult bindingResult) throws ExecutionException, InterruptedException {
+        if (bindingResult.hasErrors()) {
+            System.out.println("error");
+            return "Error";
+        } else {
+            System.out.println();
+            System.out.println(cart);
+            return cartService.createCart(cart);
+        }
+    }
+
+    @DeleteMapping("/deleteC")
+    public String deleteCart(@RequestBody String cartId) throws ExecutionException, InterruptedException {
+        return cartService.deleteCart(cartId);
+    }
+
+    @PutMapping("/alterarC")
+    public String updateCart(@RequestBody Cart cart) throws ExecutionException, InterruptedException {
+        return cartService.updateCart(cart);
+    }
+}
